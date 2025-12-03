e1cb8c56c3bbfb11273b1cb360d5dfdc2fb9ec30
hztaxx <2979777560@qq.com>
2025-12-03 15:55:44 +0800

    练习: w1d1-1

---
+++ basics/w1/d1/w1d1-1.py
@@ -0,0 +1,19 @@
+# w11-1.py
+print('i\'m ok.')
+print('i\'m learning\npython')
+print('\\n\\')
+print('\\\n\\')
+print('\\\t\\')
+print(r'\\\t\\')
+print('''line1
+      ...line2
+      ...line3''')
+print('''line1
+...line2
+...line3''')
+print('''line1
+line2
+line3''')
+print(r'''hellp,\n
+      world''')
+
