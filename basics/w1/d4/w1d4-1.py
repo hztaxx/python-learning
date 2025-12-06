3e21f0017b46b2aec87163a9b2a73a0c51713ae1
hztaxx <2979777560@qq.com>
2025-12-06 13:00:36 +0800

    2025-12-06 | 第四天：循环与模式匹配

--- basics/w1/d4/w1d4-1.py
+++ basics/w1/d4/w1d4-1.py
@@ -1,1 +1,27 @@
 # w1d4-1.py
+h=173
+match h:
+    case x if x<=140:
+        print('not very tall')
+    case y if y<=160:
+        print('not bad')
+    case z if z<=170:
+        print('it\'s ok')
+    case 171|172|173|174|175:
+        print('good')
+    case 176|177|178|179|180:
+        print('very good')
+    case t if t<=190:
+        print('so good')
+    case g if g<191:
+        print('perfect')
+color = ['red','black','green']
+match color:
+    case list() if 'black' in color:
+        print('not good')
+    case['red',file1,*files]:
+        print('so much colors')
+    case['red']:
+        print('good')
+    case _:
+        print('well')
--- basics/w1/d4/w1d4-2.py
+++ basics/w1/d4/w1d4-2.py
@@ -1,1 +1,32 @@
 # w1d4-2.py
+names = ['Michael', 'Bob', 'Tracy']
+for name in names:
+    print(name)
+sum = 0
+for x in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+    sum = sum + x
+print(sum)
+x=list(range(5))
+print(x)
+sum = 0
+for x in range(101):
+    sum = sum + x
+print(sum)
+L = ['Bart', 'Lisa', 'Adam']
+for x in range(3):
+    print(L[x])
+
+n = 1
+while n <= 100:
+    if n > 10: # 当n = 11时，条件满足，执行break语句
+        break # break语句会结束当前循环
+    print(n)
+    n = n + 1
+print('END')
+n = 0
+while n < 10:
+    n = n + 1
+    if n % 2 == 0: # 如果n是偶数，执行continue语句
+        continue # continue语句会直接继续下一轮循环，后续的print()语句不会执行
+    print(n)
+
