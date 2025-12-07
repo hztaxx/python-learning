8946e8476f5c11e6e870223ebf88613f7253d88e
hztaxx <2979777560@qq.com>
2025-12-07 10:29:32 +0800

    2025-12-07 | 第五天：内置函数与数据结构进阶

--- basics/w1/d5/w1d5-1.py
+++ basics/w1/d5/w1d5-1.py
@@ -1,1 +1,24 @@
 # w1d5-1.py
+f={'kfc':50,'mcd':40, "bk":30}
+print(f['bk'])
+f['tst']=20
+print(f['tst'])
+if 'bsk' in f:
+    print('y')
+else : 
+    print('n')
+x=f.get('kfc')
+print(x)
+y=f.get('kfj')
+print(y)
+y=f.get('kfj','gg')
+print(y)
+s={1,2,3}
+s2=set([1,2,3])
+print(s2)
+s={1,2,3,1,2,1,2,4,2,6}
+print(s)
+s.remove(6)
+print(s)
+print(s2 & s)
+print(s2|s)
--- basics/w1/d5/w1d5-2.py
+++ basics/w1/d5/w1d5-2.py
@@ -1,1 +1,8 @@
 # w1d5-2.py
+print(abs(-444))
+print(int(5123.24))
+print(float('33.3'))
+n1 = 255
+n2 = 1000 
+print(str(hex(n1))) 
+print(str(hex(n2)))
